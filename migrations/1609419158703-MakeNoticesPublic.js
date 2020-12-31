9664824a019cd197ee3209bb934c91efbe56c77b
Fatih Kaya <1994274@gmail.com>
2020-12-31 17:14:08 +0300

    allow non-authenticated users to access to the notice images

--- migrations/1609419158703-MakeNoticesPublic.js
+++ migrations/1609419158703-MakeNoticesPublic.js
@@ -6,10 +6,25 @@ const database = require('directus/dist/database')
 module.exports.up = function (next) {
   database.schemaInspector.overview().then(async schema => {
     const permissions = new directus.PermissionsService({ schema })
+    const relationsService = new directus.RelationsService({ schema })
+    const imagesRelation = await relationsService.readSingleton({
+      filter: {
+        one_collection: 'notices',
+        one_field: 'images'
+      }
+    })
 
     await permissions.create({
       role: null,
       collection: 'notices',
+      action: 'read',
+      fields: '*'
+    })
+
+    // images table can be read by all the users
+    await permissions.create({
+      role: null,
+      collection: imagesRelation.many_collection,
       action: 'read',
       fields: '*'
     })
@@ -21,10 +36,26 @@ module.exports.up = function (next) {
 module.exports.down = function (next) {
   database.schemaInspector.overview().then(async schema => {
     const permissionsService = new directus.PermissionsService({ schema })
+    const relationsService = new directus.RelationsService({ schema })
+    const imagesRelation = await relationsService.readSingleton({
+      filter: {
+        one_collection: 'notices',
+        one_field: 'images'
+      }
+    })
 
     await permissionsService.deleteByQuery({
       filter: {
         collection: 'notices',
+        role: { _null: true },
+        action: 'read',
+        fields: '*'
+      }
+    })
+
+    await permissionsService.deleteByQuery({
+      filter: {
+        collection: imagesRelation.many_collection,
         role: { _null: true },
         action: 'read',
         fields: '*'
